1f6b49574e01d3a0379dbd24a8040862eca81456
mksmith1982 <mksmith1982@gmail.com>
2016-10-23 21:30:42 -0400

    most recent commit finished majority of script file

---
+++ src/js/main.js
@@ -0,0 +1,97 @@
+import $ from "jquery";
+
+import { data } from "./items.js";
+
+// console.log(data);
+
+
+
+
+
+   function getData(data){
+	var dataResults = [];
+	for (var i = 0; i < data.results.length; i++) {
+		
+		var itemName = data.results[i].title;
+		var itemPrice = data.results[i].price;
+		var storeName = data.results[i].Shop.shop_name;
+		var storeLink = data.results[i].url;
+		var image = data.results[i].Images[0].url_170x135;
+		var itemLink = data.results[i].url;
+
+	
+	
+
+
+// get the data structured how you want it on the page:
+
+  var resultHTML = 
+
+  
+`<div class = "singleItem">
+      <img src = ${image}>
+      
+    <div>
+        
+        	<a href = ${itemLink}>
+        	${itemName}
+        </a>
+         
+ 			<div>
+          
+
+				<span class = "store"> 
+				<a href =${storeLink}>${storeName} </a>
+				</span>
+          
+				<span class = "price">
+				$ ${itemPrice} 
+				</span>	
+
+            </div>
+  
+       </div>
+</div>`;
+
+// get the data onto the page:
+
+    $(".dataResults").append(resultHTML);
+
+}
+};
+
+
+// run the function we just wrote.
+
+getData(data);
+
+
+// variable that pulls the data
+
+// variable the 
+
+
+// pull data 
+
+// loop through items..
+
+
+// name items a var
+
+// data.results[i]
+
+
+// [0] runs through first results
+
+
+
+// I have to pull data with a function loop
+
+// var itemname data.results[i].title
+
+
+// function getData (data)
+// var data=[];
+// for (var i = 0; i < Things.length; i++) {
+// 	Things[i]
+// }
